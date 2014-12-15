6f9d966a025ededa6cced32bc048824b968d0cef
Ayako <ayomori@hotmail.com>
2014-12-15 21:25:04 +0900

    Keynote タイトルアップデート、セッションリンク修正

--- event/goazure/js/tdLink.js
+++ event/goazure/js/tdLink.js
@@ -1,12 +1,6 @@
 
 $('td[data-href]').addClass('clicklink').click(function(e) {
-  var target_offset = $($(this).attr('data-href')).offset(),target_top = target_offset.top -145;
+  var target_offset = $($(this).attr('data-href')).offset(),target_top = target_offset.top;
   $('html, body').animate({scrollTop:target_top}, 200);
 });
 
-$(document).ready(function(){
-		if(location.hash != ""){
-  var target_offset = $(location.hash).offset(),target_top = target_offset.top -145;
-  $('html, body').animate({scrollTop:target_top}, 200);
-		}
-});
